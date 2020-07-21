981ecc87215cf17648f26ec1fdce65301940ff65
Sunbreak <sunbreak.wang@gmail.com>
2020-07-21 19:59:00 +0800

    Tour for checked_yaml
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

---
+++ checked_yaml_tour/bin/main.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:checked_yaml/checked_yaml.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'main.g.dart';
+
+@JsonSerializable(
+  anyMap: true,
+  checked: true,
+  disallowUnrecognizedKeys: true,
+  nullable: false,
+)
+class Configuration {
+  @JsonKey(required: true)
+  final String name;
+  @JsonKey(required: true)
+  final int count;
+
+  Configuration({this.name, this.count}) {
+    if (name.isEmpty) {
+      throw ArgumentError.value(name, 'name', 'Cannot be empty.');
+    }
+  }
+
+  factory Configuration.fromJson(Map json) => _$ConfigurationFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ConfigurationToJson(this);
+
+  @override
+  String toString() => 'Configuration: ${toJson()}';
+}
+
+void main(List<String> arguments) {
+  var sourcePathOrYaml = arguments.single;
+  String yamlContent;
+
+  if (FileSystemEntity.isFileSync(sourcePathOrYaml)) {
+    yamlContent = File(sourcePathOrYaml).readAsStringSync();
+  } else {
+    yamlContent = sourcePathOrYaml;
+    sourcePathOrYaml = null;
+  }
+
+  final config = checkedYamlDecode(
+      yamlContent, (m) => Configuration.fromJson(m),
+      sourceUrl: sourcePathOrYaml);
+  print(config);
+}
